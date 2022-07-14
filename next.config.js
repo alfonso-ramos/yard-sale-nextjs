031ddceeac71e778f61d0d8e8ac5a6c60103bc1f
Alfonso <alfonsoramos.1803@gmail.com>
2022-07-14 17:57:39 -0600

    [ADD] Agregados alias para las rutas y conexion con los estilos

--- next.config.js
+++ next.config.js
@@ -2,20 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  env: {
-    customKey: 'customValue',
-  },
-  basePath: '/dist',
-  compress: true,
-  async redirects(){
-    return[
-      {
-        source: '/',
-        destination: '/',
-        permanent: true,
-      }
-    ]
-  }
 }
 
 module.exports = nextConfig
---
+++ src/components/OrderItem.jsx
@@ -0,0 +1,30 @@
+import { useContext } from 'react';
+import AppContext from '@context/AppContext';
+
+import CloseIcon from '@icons/icon_close.png'
+
+import styles from '@styles/OrderItem.module.scss'
+
+const OrderItem = (props) => {
+    const {product, indexValue} = props
+    const { removeFromCart } = useContext(AppContext)
+
+    const handleRemove = index => {
+        removeFromCart(index)
+    }
+    return (
+        <div className={styles.OrderItem}>
+            <figure>
+                <img
+                    src={product.images[0]}
+                    alt={product.title}
+                />
+            </figure>
+            <p>{product.title}</p>
+            <p>${product.price}</p>
+            <img src={CloseIcon} alt="close" onClick={() => handleRemove(indexValue)}/>
+        </div>
+    );
+};
+
+export default OrderItem;

1ea9397f89254a4120aa5ac4017199c15f3fd1c2
Alfonso <alfonsoramos.1803@gmail.com>
2022-07-13 19:36:53 -0600

    [ADD] configuracion del next.config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,20 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  env: {
+    customKey: 'customValue',
+  },
+  basePath: '/dist',
+  compress: true,
+  async redirects(){
+    return[
+      {
+        source: '/',
+        destination: '/',
+        permanent: true,
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
